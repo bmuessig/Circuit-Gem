09bffc542fe68e9caf9542ab11666b90111f5d51
Benedikt Müssig <github@bmuessig.eu>
2022-03-31 02:55:20 +0200

    Changed the base image to retropie 4.4

--- build/build-retro.sh
+++ build/build-retro.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-IMAGE_OUT=retropie-4.8.img
+IMAGE_OUT="retropie-4.4.img"
 IMAGE_OUT_GZ="${IMAGE_OUT}".gz
 
 if [ ! -f "${IMAGE_OUT}" ]; then
   echo "Downloading and extracting image"
-  wget -nc -O "${IMAGE_OUT_GZ}" "https://github.com/RetroPie/RetroPie-Setup/releases/download/4.8/retropie-buster-4.8-rpi1_zero.img.gz"
+  wget -nc -O "${IMAGE_OUT_GZ}" "https://github.com/RetroPie/RetroPie-Setup/releases/download/4.4/retropie-4.4-rpi1_zero.img.gz"
   gunzip "${IMAGE_OUT_GZ}"
 fi
 
